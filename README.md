f5722509382ae6044ad1118455f4febbd47f77d5
CorentinTh <corentin.thomasset74@gmail.com>
2018-06-12 13:49:34 +0200

    First commi

---
+++ README.md
@@ -0,0 +1,84 @@
+# browser-i18n
+
+A simple in-browser i18n module, compatible with the [i18n-node](https://github.com/mashpie/i18n-node) server module data files. Meant to be use with a module bundler (like [parcel](https://parceljs.org)).
+
+## Usage
+
+```javascript
+import I18n from 'browser-i18n';
+
+const i18n = new I18n({
+    language: 'fr',
+    path: '/locales',
+    extension: '.json'
+});
+
+console.log( i18n.__('Hello') );
+// Output: 'Bonjour'
+
+console.log( i18n.__('Oh, hi %s!', 'Mark') );
+// Output: 'Oh, salut Mark!'
+
+
+// Or using the global selector ...
+
+console.log( __('Hello') );
+// Output: 'Bonjour'
+```
+
+
+### Files structures
+
+One file for each language. They may have the following structure:
+
+```json
+// /locales/en.json
+
+{
+  "Hello": "Hello",
+  "Oh, hi %s!": "Oh, hi %s!"
+  "Bye!": "Bye!"
+}
+```
+```json
+// /locales/fr.json
+
+{
+  "Hello": "Bonjour",
+  "Oh, hi %s!": "Oh, salut %s!"
+  "Bye!": "Au revoir!"
+}
+```
+
+Put your locales folder accessible publicly. You can do it by putting it in your public root:
+
+```
+.
+└── public
+    └── locales
+        ├── en.json
+        └── fr.json
+```
+
+Or, using express:
+
+```javascript
+app.use('/locales', express.static(path.join(__dirname, 'locales')));
+```
+
+## API
+
+Configuration:
+
+```javascript
+const i18n = new I18n({
+    language: 'fr',     // The langage wanted - Default 'en'
+    path: '/locales',   // The path to access the locales files - Default '/locales'
+    extension: '.json', // Local file extension - Default '.json'
+    setGobal: true,     // Set the function '__' on a global scope - Default true
+    onReady: callback,  // Set a callback triggered when the dataFile is loaded
+    verbose: true       // Set the verbosity of the object - Default to true
+});
+```
+
+
---
+++ src/index.js
@@ -0,0 +1,118 @@
+import {vsprintf} from 'sprintf-js'
+
+export default class I18n {
+
+    /**
+     * Create a new instance of the I18n class
+     * @param {object} config
+     * @param {string} config.language - Local language to apply
+     * @param {string} config.path - Path on the server to access language files
+     * @param {string} config.extension - Language files extension
+     * @param {boolean} config.setGlobal - Set the function __ as a globally accessible variable
+     * @param {function} config.onReady - Callback triggered when the datafile are gathered
+     * @param {boolean} config.verbose - Disable/enable console logging
+     */
+    constructor(config) {
+        this._parseConfig(config);
+        this._setGlobal();
+        this._setVerbosity();
+        this._getFileFromServer();
+
+        this.callbacks = [];
+    }
+
+    /**
+     * @param config
+     * @private
+     */
+    _parseConfig(config) {
+        this.config = {
+            language: config.language || 'en',
+            path: config.path || '/locales',
+            extension: config.extension || '.json',
+            setGlobal: config.setGlobal || true,
+            onReady: config.onReady || function () {},
+            verbose: config.verbose || true
+        };
+    }
+
+    /**
+     * Retrieve file from server
+     * @private
+     */
+    _getFileFromServer() {
+        $.ajax({
+            url: this.config.path + '/' + this.config.language + this.config.extension,
+            dataType: 'json',
+            async: false,
+            success: this._onSuccess.bind(this),
+            error: this._onError.bind(this)
+        })
+    }
+
+    /**
+     * Set the function __ on a global scope
+     * @private
+     */
+    _setGlobal() {
+        if (this.config.setGlobal) {
+            window.__ = this.__.bind(this);
+        }
+    }
+
+    /**
+     * Set the logger for the given verbosity
+     * @private
+     */
+    _setVerbosity() {
+        this._logger = {
+            info: () => {},
+            error: () => {},
+            log: () => {},
+        };
+
+        if(this.config.verbose){
+            this._logger = {
+                info: console.info,
+                error: console.error,
+                log: console.log,
+            }
+        }
+    }
+
+    /**
+     * Datafile successfully gathered from the server
+     * @param data
+     * @private
+     */
+    _onSuccess(data) {
+        this._logger.info('[browser-i18n] File loaded successfully.');
+
+        this.localeFile = data;
+        this.config.onReady(data);
+    }
+
+    /**
+     * Error while retrieving datafile from server
+     * @private
+     */
+    _onError() {
+        this._logger.error('[browser-i18n] Cannot get the file.');
+    }
+
+    /**
+     * Get a translation
+     * @param {string} text
+     * @param {string} [args] - Optional arguments
+     * @returns {string}
+     */
+    __(text) {
+        let textTranslated = this.localeFile[text];
+
+        if (arguments.length > 1) {
+            textTranslated = vsprintf(textTranslated, Array.prototype.slice.call(arguments, 1));
+        }
+
+        return textTranslated;
+    }
+};
